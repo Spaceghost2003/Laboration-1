b0902dbd3db1d2e958b0f0a20e2a2aca4afbd5fa
Jonathan Olaussen Alvin <jonathanoalvin@gmail.com>
2024-09-13 17:04:11 +0200

    Add project files.

---
+++ Laboration 1/Program.cs
@@ -0,0 +1,59 @@
+﻿introSplash();
+
+Console.WriteLine("Skriv in en sträng!");
+string userInput = Console.ReadLine();
+Console.WriteLine("***********************************");
+
+long sumOfNumbers = 0;
+
+for (int i = 0; i < userInput.Length; i++)
+{
+    for (int k = i; k < userInput.Length; k++)
+    {
+        if (!char.IsAsciiDigit(userInput[k]))
+        {
+            break;
+        }
+        if (userInput[i] == userInput[k] && k != i)
+        {
+            sumOfNumbers += long.Parse(userInput[i..(k + 1)]);
+
+            Console.Write(userInput[0..i]);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(userInput[i..(k + 1)]);
+            Console.ResetColor();
+            Console.Write(userInput[(k + 1)..(userInput.Length)] + "\n");
+
+            break;
+        }
+    }
+}
+Console.WriteLine("***********************************");
+Console.WriteLine($"Summan av talen är: {sumOfNumbers}");
+static void introSplash()
+{
+    Console.WriteLine(@"
+              ,---------------------------,
+              |  /---------------------\  |
+              | |                       | |
+              | |   tryck på en tangent | |
+              | |       för att         | |
+              | |   starta programmet   | |
+              | |                       | |
+              |  \_____________________/  |
+              |___________________________|
+            ,---\_____     []     _______/------,
+          /         /______________\           /|
+        /___________________________________ /  | ___
+        |                                   |   |    )
+        |  _ _ _                 [-------]  |   |   (
+        |  o o o                 [-------]  |  /    _)_
+        |__________________________________ |/     /  /
+    /-------------------------------------/|      ( )/
+  /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/ /
+/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/ /
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    Console.ReadKey();
+    Console.Clear();
+
+}
